7d8f87485633aec226c97a35ff119ef3f166f3bd
unknown <jayprakashpj2525@gmail.com>
2023-07-30 11:45:15 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,99 +24,105 @@ const Contact = () => {
       [name]: value,
     });
   };
-  
-  // 
+
+  //
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true);
+    if (form.name == "" || form.email == "" || form.message == "") {
+      alert("Fill up all the fields!!");
+    } else {
+      setLoading(true);
 
-    emailjs
-      .send(
-        'service_zroajyn',
-        'template_l9un9f6',
-        {
-          from_name: form.name,
-          to_name: "Jay",
-          from_email: form.email,
-          to_email: "jpnreddy25@gmail.com",
-          message: form.message,
-        },
-'sE-s_AZMedbPMZx18'
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+      emailjs
+        .send(
+          "service_zroajyn",
+          "template_l9un9f6",
+          {
+            from_name: form.name,
+            to_name: "Jay",
+            from_email: form.email,
+            to_email: "jpnreddy25@gmail.com",
+            message: form.message,
+          },
+          "sE-s_AZMedbPMZx18"
+        )
+        .then(
+          () => {
+            setLoading(false);
+            alert("Thank you. I will get back to you as soon as possible.");
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          },
+          (error) => {
+            setLoading(false);
+            console.error(error);
 
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+            alert("Ahh, something went wrong. Please try again.");
+          }
+        );
+    }
   };
 
   return (
-    <div
-      className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'
-    >
+    <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
-        <p className='text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]'>Ring me now!</p>
-        <h3 className='font-black text-white lg:text-[60px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2'>Contact</h3>
+        <p className="text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]">
+          Ring me now!
+        </p>
+        <h3 className="font-black text-white lg:text-[60px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
+          Contact
+        </h3>
 
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className="mt-12 flex flex-col gap-8"
         >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
             <input
-              type='text'
-              name='name'
+              type="text"
+              name="name"
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your email</span>
             <input
-              type='email'
-              name='email'
+              type="email"
+              name="email"
               value={form.email}
               onChange={handleChange}
               placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
               rows={7}
-              name='message'
+              name="message"
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              placeholder="What you want to say?"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
             />
           </label>
 
           <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            type="submit"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
             {loading ? "Sending..." : "Send"}
           </button>
@@ -125,9 +131,8 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-      </motion.div>
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      ></motion.div>
     </div>
   );
 };
